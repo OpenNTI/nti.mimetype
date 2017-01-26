9b2468fee7b00e574ea01c9a55fc6ecc07aec9d3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-26 17:55:02 +0000

    move test to proper module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.mimetype/trunk@105249 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/mimetype/tests/test_module.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import assert_that
+
+import unittest
+
+from nti.mimetype import guess_type
+
+
+class TestMimeTypes(unittest.TestCase):
+
+    def test_guess_type(self):
+        t = guess_type('foo.xml')
+        assert_that(t[0], is_('application/xml'))
+        t = guess_type('foo.mml')
+        assert_that(t[0], is_('text/mathml'))
+        assert_that(guess_type(None), is_((None, None)))
+
+    def test_image_types(self):
+        assert_that(guess_type('foo.png')[0], is_('image/png'))
+        assert_that(guess_type('foo.jpg')[0], is_('image/jpeg'))
+        assert_that(guess_type('foo.jpeg')[0], is_('image/jpeg'))
+        assert_that(guess_type('foo.bmp')[0], is_('image/bmp'))
+        assert_that(guess_type('foo.gif')[0], is_('image/gif'))
+        assert_that(guess_type('foo.tif')[0], is_('image/tiff'))
+        assert_that(guess_type('foo.tiff')[0], is_('image/tiff'))
+        assert_that(guess_type('foo.jp2')[0], is_('image/jp2'))
